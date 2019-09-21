4e2b0ecb484e13e2f96934a142261e72c9be8fba
automation <atcsurya@gmail.com>
2019-09-21 08:54:00 +0530

    comment demo class added

---
+++ src/basic_concepts/CommentsDemo.java
@@ -0,0 +1,18 @@
+package basic_concepts;
+
+/**
+ * this program explains comments in Java
+ * There are 3 types of comments
+ * single line comment 
+ * multi line comments
+ * documentation comments
+ */
+
+public class CommentsDemo {
+	// single line comment
+
+	/*
+	 * this is an example to the
+	 * multiline comment
+	 */
+}
