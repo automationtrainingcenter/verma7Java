f29364e82267b240d86a2d090cc0f88794d8751b
automation <atcsurya@gmail.com>
2019-09-25 08:40:47 +0530

    static demo and data types theory

---
+++ src/basic_concepts/DataTypesDemo.java
@@ -0,0 +1,49 @@
+package basic_concepts;
+/*
+* In java we have mainly two types of data types
+* 1. primitive
+* 		boolean either true or false - Boolean
+* 		char which will store single character - Character
+*		byte store integer values - Byte
+*		short store integer values - Short
+*		int store integer values -Integer
+*		long store integer values - Long
+*		float store floating point number -Float
+*		double store floating point number - Double
+* 	
+* 2. reference
+* 		Arrays
+* 		Strings
+* 
+*  Wrapper class: For every primitive data_type we have an equivalent class in Java. Those classes
+* are known as Wrapper classes. These are used to perform different operations on data.
+* 		1. Boolean
+* 		2. Character
+* 		3. Byte
+* 		4. Short
+* 		5. Integer
+* 		6. Long
+* 		7. Float
+* 		8. Double
+* 
+* 	WCData_type obj_name = new WCData_type(value);
+* 
+* 	Type Casting: converting one type of primitive data to another type of primitive data
+* 	up casting: lower memory type to higher memory type : widening : no data loss
+* 	down casting: higher memory data type to lower memory data type : narrowing : data loss is possible
+* 
+* Converting primitive data type to wrapper class: boxing
+*  Converting wrapper to primitive : un-boxing
+* 
+* 
+* syntax to create primitive data type
+* data-type var-name  = value; //initialization
+* data-type var-name; //declaration
+* var-name = value; //assignment to the variable which is already declared
+* 
+* variable is nothing but name of memory location where we are storing data
+*/
+
+public class DataTypesDemo {
+
+}
