eb32319c11d5b0189c075fa9ede751ad54b497a3
automation <atcsurya@gmail.com>
2019-10-03 08:48:20 +0530

    operators and conditional statements theory

---
+++ src/basic_concepts/ConditionalStatements.java
@@ -0,0 +1,77 @@
+package basic_concepts;
+/*These are used to execute a block of statements based on some condition
+ * ******** if ************
+ * if(expression){
+ * 		statements
+ * }
+ * if the expression true then the statements inside if block will be executed.
+ * 
+ * 
+ * ************ if-else ************
+ * if(expression){
+ * 		statements;
+ * }else{
+ * 		statements;
+ * }
+ * if the expression is true the statements inside if block will be executed else statements 
+ * inside else block will be executed
+ * 
+ * 
+ * ************ else if ladder ************
+ * if(expression 1){
+ * 		statements;
+ * }else if(expression 2){
+ * 		statements;
+ * }else if(expression 3){
+ * 		statements;
+ * }
+ * .
+ * .
+ * .
+ * }else if(expression n){
+ * 		statements;
+ * }else{
+ * 		statements;
+ * }
+ * if expression 1 is true it will execute statements inside if block else if will expression 2 and 
+ * if expression 2 is true it will execute statements inside expression 2 else if block else if will
+ * verify next expression. if all the expressions are false then it will execute statements inside
+ * else block
+ * 
+ * 
+ * ************ nested if ************
+ * if(expression 1){
+ * 		if(expression 2{
+ * 			statements;
+ * 		}else{
+ * 			statements;
+ * 		}
+ * }else{
+ * 		statements;
+ * }
+ * 
+ * 
+ * ************ switch case: it will work based on value ************
+ * switch(Value){
+ * 		case value1;
+ * 			statements;
+ * 			break;
+ * 		case value2:
+ * 			statements;
+ * 			break;
+ * 		.
+ * 		.
+ * 		.
+ * 		case valueN:
+ * 			statements;
+ * 			break;
+ * 		default:
+ * 			statements;
+ * }
+ * 	
+ * 
+ */
+
+public class ConditionalStatements {
+
+}
